3d0f9d6574df2c81e533f5d2c86b56b71d21adc6
Zach Magloughlin <zmagloughlin@mines.edu>
2024-09-22 15:25:46 -0600

    Tweaked heading, rebuilt showcase styling. Trying to get navbar to be on the same baseline

--- webpage/navbar.php
+++ webpage/navbar.php
@@ -9,7 +9,7 @@
     <ul id="navbar">
         <li><a href="home.php" class="">Home</a></li>
         <li><a href="published_work.php" class="">Published Work</a></li>
-        <li><a href="all_work.php" class="">All Works</a></li>
+        <li><a href="all_work.php" class="">All Work</a></li>
         <li><a href="contact.php" class="">Contact</a></li>
     </ul>
 </nav>
--- webpage/showcase.php
+++ webpage/showcase.php
@@ -6,14 +6,9 @@
 </head>
 
 <section id="carousel_container">
-    <div class="carousel">
-        <img src="resources/images/placeholder.png" class="carousel_image"> </img>
-        <img src="resources/images/placeholder.png" class="carousel_image"> </img>
-        <img src="resources/images/placeholder.png" class="carousel_image"> </img>
-    </div>
-
-    <span class="arrow_container">
-        <button class="arrows" id="left_arrow" type="image" src="resources/images/left-arrow.png" />
-        <button class="arrows" id="right_arrow" type="image" src="resources/images/right-arrow.png" />
-    </span>
+    <button class="arrows" id="left_arrow" type="image" src="resources/images/left-arrow.png"> </button>
+    <img class="carousel_image left" src="resources/images/placeholder.png"> </img>
+    <img class="carousel_image middle" src="resources/images/placeholder.png"> </img>
+    <img class="carousel_image right" src="resources/images/placeholder.png"> </img>
+    <button class="arrows" id="right_arrow" type="image" src="resources/images/right-arrow.png"> </button>
 </section>
