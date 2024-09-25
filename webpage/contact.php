8dbaf6c755da09b7b10352945e8953c5ada92978
Zach Magloughlin <zmagloughlin@mines.edu>
2024-09-25 16:34:51 -0600

    Re-added old contact information into Contact page.

--- webpage/contact.php
+++ webpage/contact.php
@@ -9,8 +9,15 @@
 <body>
     <?php include 'common.php'; ?>
     
+    <div id="contact">
+        <div id="intro">
+            <p> To contact me, please email <a href="mailto:magloughlin.zach@gmail.com">magloughlin.zach@gmail.com</a> or use the form below. </p>
+            <p> This form is for illustration purposes only. </p>
+        </div>
+    </div>
+    
     <?php include 'under_construction.php'; ?>
-
+    
     <?php include 'footer.php'; ?>
 </body>
 
