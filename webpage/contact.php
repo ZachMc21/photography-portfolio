a81f9a1a2131fe83ca8f803e4e254d2e16b3fc94
Zach Magloughlin <zmagloughlin@mines.edu>
2024-09-25 16:56:29 -0600

    Contact form skeleton is in

--- webpage/contact.php
+++ webpage/contact.php
@@ -15,8 +15,24 @@
             <p> This form is for illustration purposes only. </p>
         </div>
     </div>
-    
+
     <?php include 'under_construction.php'; ?>
+    
+    <form>
+        <fieldset id="contact_me">
+            <legend>Contact Me!</legend>
+            <label for="first_name">First name:</label>
+            <input type="text" id="first_name" name="first_name" required><br>
+            <label for="last_name">Last name:</label>
+            <input type="text" id="last_name" name="last_name"><br>
+            <label for="email">Email:</label>
+            <input type="email" id="email" name="email" required><br>
+            <label for="message">Message:</label>
+            <input type="text" id="message" name="message" required><br>
+            <input type="submit" value="Submit">
+        </fieldset>
+    </form>
+
     
     <?php include 'footer.php'; ?>
 </body>
